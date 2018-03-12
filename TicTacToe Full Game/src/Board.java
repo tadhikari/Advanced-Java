6e6f17d67630e26e8a1c3a0db002d050f25caa54
Tribikram Adhikari <tribikram.adhikari@hotmail.com>
2018-03-12 10:05:44 -0500

    game finsihed

--- TicTacToe Full Game/src/Board.java
+++ TicTacToe Full Game/src/Board.java
@@ -10,6 +10,8 @@ public class Board extends JPanel implements MouseListener{
 	GameLogic gamePlay;
 	ArrayList<Object> symbol;
 	boolean p1;
+	String currentSymbol;
+	String temp;
 	
 	public Board() {
 		
@@ -20,7 +22,10 @@ public class Board extends JPanel implements MouseListener{
 		gamePlay = new GameLogic();
 		symbol = new ArrayList<Object>();
 		p1 = true;
-		
+		String[] symbols = { "X","O" };
+		currentSymbol = (String) JOptionPane.showInputDialog(null, "Player 1 select your symbol",
+		        "Starting Symbol", JOptionPane.QUESTION_MESSAGE, null, symbols, "X");
+		temp="";
 	}
 	
 	@Override
@@ -44,13 +49,53 @@ public class Board extends JPanel implements MouseListener{
 		//symbol.add(new Object[] {"X",200,200});
 		
 		for(int i =0;i<symbol.size();i++) {
-			
 			Object[] a = (Object[]) symbol.get(i);
 			g.drawString((String) a[0], (int) a[1], (int)a[2]);
-			
+		}
+		
+		g.setColor(Color.BLUE);
+		
+		if(temp.equals("1,5,9")) {
+			g.drawLine(450, 475, 150, 175); //diagnol
+		}
+		
+		else if(temp.equals("7,5,3")) {
+			g.drawLine(150, 475, 450, 175); //diagnol
+		}
+		
+		else if(temp.equals("2,5,8")) {
+			g.drawLine(300, 475, 300, 175); //vertical middle
+		}
+		
+		else if(temp.equals("3,6,9")) {
+			g.drawLine(380, 475, 380, 175); //verticle right
+		}
+		
+		else if(temp.equals("1,4,7")) {
+			g.drawLine(225, 475, 225, 175); //verticle left
 		}
 		
 		
+		else if(temp.equals("1,2,3")) {
+			g.drawLine(500, 240, 117, 240); //horizontal top
+		}
+		
+		else if(temp.equals("4,5,6")) {
+			g.drawLine(500, 330, 117, 330); //horizontal middle
+		}
+		
+		else if(temp.equals("7,8,9")) {
+			g.drawLine(500, 410, 117, 410); //horizontal bottom
+		}
+			
+		
+			
+//		else if(temp.equals("2,5,8")) {
+//			g.drawLine(300, 475, 300, 175);
+//		}
+		
+		
+//		g.drawLine(350, 375, 250, 100);
 		
 //		g.drawString("X", 210, 260); //1,1
 //		g.drawString("X", 285, 260); //1,2
@@ -91,64 +136,120 @@ public class Board extends JPanel implements MouseListener{
 	@Override
 	public void mouseReleased(MouseEvent e) {
 		
-		String current_Symbol;
-	
+		String current_Symbol = this.currentSymbol;
+		
+		int p;
+		
 		if(p1) {
-			
-			current_Symbol = "X";
-			
+			p = 1;
 		}
 		
 		else {
-			current_Symbol = "O";
+			p = 2;
 		}
 		
 		System.out.println(e.getX() + " " + e.getY());
-		if(e.getX() >= 118 && e.getX()<=248 && e.getY()>=151 && e.getY()<=273) {
+		if(e.getX() >= 118 && e.getX()<=248 && e.getY()>=151 && e.getY()<=273 && gamePlay.isFree(0)) {
 			System.out.println("1,1");
+			gamePlay.play("1,1", p);
 			symbol.add(new Object[] {current_Symbol,210,260});
 			p1 = !p1;
+			repaint();
+			youWin(p);
 		}
 		
-		else if(e.getX() >= 252 && e.getX() <= 349 && e.getY() >= 151 && e.getY() <= 273) {
+		else if(e.getX() >= 252 && e.getX() <= 349 && e.getY() >= 151 && e.getY() <= 273 && gamePlay.isFree(1)) {
 			System.out.println("1,2");
+			gamePlay.play("1,2", p);
+			symbol.add(new Object[] {current_Symbol,285,260});
+			p1 = !p1;
+			repaint();
+			youWin(p);
 		}
 		
-		else if(e.getX() >= 351 && e.getX() <= 501 && e.getY() >= 150 && e.getY() <= 273) {
-			System.out.println("1,3");
+		else if(e.getX() >= 351 && e.getX() <= 501 && e.getY() >= 150 && e.getY() <= 273 && gamePlay.isFree(2)) {
+			gamePlay.play("1,3", p);
+			symbol.add(new Object[] {current_Symbol,365,260});
+			p1 = !p1;
+			repaint();
+			youWin(p);
 		}
 		
-		else if(e.getX()>=118 && e.getX()<=248 && e.getY()>= 274 &&e.getY()<=373) {
+		else if(e.getX()>=118 && e.getX()<=248 && e.getY()>= 274 &&e.getY()<=373 && gamePlay.isFree(3)) {
 			System.out.println("2,1");
+			gamePlay.play("2,1", p);
+			symbol.add(new Object[] {current_Symbol,210,350});
+			p1 = !p1;
+			repaint();
+			youWin(p);
 		}
 		
-		else if(e.getX() >= 252 && e.getX() <= 349 && e.getY() >= 274 && e.getY()<=373) {
+		else if(e.getX() >= 252 && e.getX() <= 349 && e.getY() >= 274 && e.getY()<=373 && gamePlay.isFree(4)) {
 			System.out.println("2,2");
+			gamePlay.play("2,2", p);
+			symbol.add(new Object[] {current_Symbol,285,350});
+			p1 = !p1;
+			repaint();
+			youWin(p);
 		}
 		
-		else if(e.getX() >= 351 && e.getX() <= 501 && e.getY() >= 274 && e.getY()<=373) {
+		else if(e.getX() >= 351 && e.getX() <= 501 && e.getY() >= 274 && e.getY()<=373 && gamePlay.isFree(5)) {
 			System.out.println("2,3");
+			gamePlay.play("2,3", p);
+			symbol.add(new Object[] {current_Symbol,365,350});
+			p1 = !p1;
+			repaint();
+			youWin(p);
 		}
 		
-		else if(e.getX()>=118 && e.getX()<=248 && e.getY()>= 374 &&e.getY()<=500) {
+		else if(e.getX()>=118 && e.getX()<=248 && e.getY()>= 374 &&e.getY()<=500 && gamePlay.isFree(6)) {
 			System.out.println("3,1");
+			gamePlay.play("3,1", p);
+			symbol.add(new Object[] {current_Symbol,210,430});
+			p1 = !p1;
+			repaint();
+			youWin(p);
 		}
 		
-		else if(e.getX() >= 252 && e.getX() <= 349 && e.getY() >= 374 && e.getY()<=500) {
+		else if(e.getX() >= 252 && e.getX() <= 349 && e.getY() >= 374 && e.getY()<=500 && gamePlay.isFree(7)) {
 			System.out.println("3,2");
+			gamePlay.play("3,2", p);
+			symbol.add(new Object[] {current_Symbol,285,430});
+			p1 = !p1;
+			repaint();
+			youWin(p);
 		}
 		
 		
-		else if(e.getX() >=351 && e.getX() <= 501 && e.getY() >= 374 && e.getY()<=500) {
+		else if(e.getX() >=351 && e.getX() <= 501 && e.getY() >= 374 && e.getY()<=500 && gamePlay.isFree(8)) {
 			System.out.println("3,3");
+			gamePlay.play("3,3", p);
+			symbol.add(new Object[] {current_Symbol,365,430});
+			p1 = !p1;
+			repaint();
+			youWin(p);
 		}
 		
+	if(currentSymbol.equals("X")) {
+		currentSymbol = "O";
+	}
+	else {
+		currentSymbol = "X";
+	}
 		
+	
 	}
 	
 	
-	public void youWin(int winner) {
-		JOptionPane.showMessageDialog(this,"Player "+winner+" wins!");
+	public void youWin(int player) {
+		if((boolean)gamePlay.playerWon(player)[0]) {
+			
+			temp = (String) gamePlay.playerWon(player)[1];
+			repaint();
+			JOptionPane.showMessageDialog(this, "Player "+player+" wins!");
+			removeMouseListener(this);
+			
+		}	
 	}
 	
 	
--- TicTacToe Full Game/src/GameLogic.java
+++ TicTacToe Full Game/src/GameLogic.java
@@ -21,45 +21,64 @@ public class GameLogic {
 		return true;
 	}
 	
+	public boolean isFree(int pos) {
+		
+		if(board[pos]==0) {
+			return true;
+		}
+		return false;
+	}
+	
 	public void play(String s, int i) {
 		
 		char[] c = s.toCharArray();
 		
-		if(c[0]=='1' && c[1]=='1' && board[0]==0) {	
+		if(s.equals("1,1") && board[0]==0) {	
 			board[0]=i;
+			System.out.println("Tribikram");
+			
 		}
 
-		else if(c[0]=='1' && c[1]=='2' && board[1]==0) {			
+		else if(s.equals("1,2") && board[1]==0) {			
 			board[1]=i;
+			
 		}
 		
-		else if(c[0]=='1' && c[1]=='3' && board[2]==0) {
+		else if(s.equals("1,3") && board[2]==0) {
 			board[2]=i;
+			
 		}
 		
-		else if(c[0]=='2' && c[1]=='1' && board[3]==0) {
+		else if(s.equals("2,1") && board[3]==0) {
 			board[3]=i;
+		
 		}
 		
-		else if(c[0]=='2' && c[1]=='2' && board[4]==0) {
+		else if(s.equals("2,2") && board[4]==0) {
 			board[4]=i;
+			
 		}
 		
-		else if(c[0]=='2' && c[1]=='3' && board[5]==0) {
+		else if(s.equals("2,3") && board[5]==0) {
 			board[5]=i;
+			
 		}
 		
-		else if(c[0]=='3' && c[1]=='1' && board[6]==0) {
+		else if(s.equals("3,1") && board[6]==0) {
 			board[6]=i;
+			
 		}
 		
-		else if(c[0]=='3' && c[1]=='2' && board[7]==0) {
+		else if(s.equals("3,2") && board[7]==0) {
 			board[7]=i;
+			
 		}
 		
-		else if(c[0]=='3' && c[1]=='3' && board[8]==0) {
+		else if(s.equals("3,3") && board[8]==0) {
 			board[8]=i;
+			
 		}
+		
 		
 	}
 	
@@ -67,24 +86,38 @@ public class GameLogic {
 	
 	
 		
-	public boolean playerWon(int i) {
+	public Object[] playerWon(int i) {
 		
-		if((board[0]==i && board[1]==i && board[2]==i) || 
-			(board[3]==i && board[4]==i && board[5]==i) ||
-			(board[6]==i && board[7]==i && board[8]==i) ||
+		if	(board[0]==i && board[1]==i && board[2]==i)  {
+			return new Object[] {true,"1,2,3"};
+		}
+		else if	(board[3]==i && board[4]==i && board[5]==i){
+			return new Object[] {true,"4,5,6"};
+		}
+		else if	(board[6]==i && board[7]==i && board[8]==i){
+			return new Object[] {true,"7,8,9"};
+		}
 			
-			(board[0]==i && board[3]==i && board[6]==i)||
-			(board[1]==i && board[4]==i && board[7]==i)||
-			(board[2]==i && board[5]==i && board[8]==i)||
-			
-			(board[0]==i && board[4]==i && board[8]==i)||
-			(board[6]==i && board[4]==i && board[2]==i)){
-			
-			return true;
+		else if	(board[0]==i && board[3]==i && board[6]==i){
+			return new Object[] {true,"1,4,7"};
+		}
+		else if	(board[1]==i && board[4]==i && board[7]==i){
+			return new Object[] {true,"2,5,8"};
+		}
+		else if	(board[2]==i && board[5]==i && board[8]==i){
+			return new Object[] {true,"3,6,9"};
+		}
+		else if	(board[0]==i && board[4]==i && board[8]==i){
+			return new Object[] {true,"1,5,9"};
+		}
+		else if	(board[6]==i && board[4]==i && board[2]==i){
+			return new Object[] {true,"7,5,3"};
+			//return new Object[] {true};
 			
 		}
 		
-		return false;
+		//return new Object[] {false};
+		return new Object[] {false,""};
 	}
 	
 	
